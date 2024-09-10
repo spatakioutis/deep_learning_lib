6e243dee08f5ff0208249970056bf7f6e8996695
sofoklis <sofoklis2314@gmail.com>
2024-09-10 19:15:29 +0300

    first commit - project basic setup

---
+++ build/CMakeFiles/deep_learning_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deep_learning_lib.dir/src/main.cpp.o"
+  "CMakeFiles/deep_learning_lib.dir/src/main.cpp.o.d"
+  "deep_learning_lib"
+  "deep_learning_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deep_learning_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/deep_learning_lib.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for deep_learning_lib.
+# This may be replaced when dependencies are built.
